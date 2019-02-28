285f854900d926d007273c2cac7a94a02128e88c
CARPINE Raphael <carpine@ad.ifsttar.fr>
2019-02-28 18:10:04 +0100

    28/02

--- test.m
+++ test.m
@@ -5,7 +5,7 @@
 % tour1.reponseLibreAvecTMD(0,1, 40);
 % tour2.reponseLibreAvecTMD([0 0],[1 0], 40);
 
-pendule = TMDpendule(9.81^2, 1, 9.81, @(theta,omega) 50*omega*(abs(theta)<5*pi/180));
+pendule = TMDpendule(9.81^2, 1, 9.81, @(theta,omega) 0*50*omega*(abs(theta)<5*pi/180));
 [t, x] = pendule.reponseLibre(pi/2-0.0001, 0, 200);
 tempsCaracteristique(t, x(:, 1))
 
@@ -14,3 +14,6 @@ tour = Structure(50, 50, @(x,v) 0.0*v, {{pendule, 1}});
 % tour.reponseLibreSansTMD(0, 1, 200);
 [t, x] = tour.reponseLibreAvecTMD(0, 1, 200);
 tempsCaracteristique(t, x(:, 1))
+
+
+animate(tour, t, x(:, 1));
