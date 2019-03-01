f1c7b11fd4fa7f4e14aa143946525af1f48be0eb
raphaelcarpine <42176262+raphaelcarpine@users.noreply.github.com>
2019-03-01 01:54:34 +0100

    '01/03'

--- animate.m
+++ animate.m
@@ -1,4 +1,4 @@
-function animate(object, t, x)
+function animate(objet, t, x)
 %animate Summary of this function goes here
 %   Detailed explanation goes here
 t = t-t(1);
@@ -7,6 +7,8 @@ N = length(t);
 deltaT = 15;
 dT = 0.04;
 gamma = t(end)/deltaT;
+
+x = x(:, 1:int16(size(x,2)/2));
 
 X = zeros(0);
 
@@ -19,23 +21,67 @@ for T=0:dT:deltaT
         break
     end
     c = (gamma*T-t(n))/(t(n+1)-t(n));
-    X(end+1) = 0.8*(x(n)*c+x(n+1)*(1-c));
+    X(end+1,:) = 0.8*(x(n,:)*c+x(n+1,:)*(1-c));
 end
 
-fig = figure('Visible', 'on');
+
+if isa(objet, 'Structure') && size(x, 1)==objet.ddl
+    lignes = 1;
+else
+    lignes = 2;
+end
+if isa(objet, 'Structure')
+    colonnes = objet.ddl;
+else
+    colonnes = 1;
+end
+
+
+fig = figure;
 axis off;
+% axis equal;
 ax = gca;
+set(ax,'DataAspectRatio',[1 1 1])
 ax.Position = ax.OuterPosition;
 hold(ax, 'on');
-xlim([-0.5 0.5]);
-ylim([-0.5 0.5]);
+xlim([-1 colonnes]);
+ylim([0.5-lignes 0.5]);
 set(fig, 'WindowButtonDownFcn', @(~, ~) show());
 
-p1 = plot(ax, 0, 0, 'o');
+updates = {};
+n = size(X, 2);
+X(1, end+1) = 0;
+for k=1:n
+    if isa(objet, 'Structure') && k<=objet.ddl
+        X(:, k) = X(:, k)/max(abs(X(:, k)));
+        draw = plot(ax, k-1, 0, 'o');
+        updatek = @(Xt) set(draw, 'XData', k-1+0.4*Xt(k));
+        updates{end+1} = updatek;
+    else
+        if isa(objet, 'Structure')
+            tmd = objet.TMDs{k-objet.ddl}{1};
+            ddl = objet.TMDs{k-objet.ddl}{2};
+        else
+            tmd = objet;
+            ddl = n+1;
+        end
+        
+        if isa(tmd, 'TMDpendule')
+            draw1 = plot(ax, [ddl-1 ddl-1], [0 -1]);
+%             draw2 = plot(ax, ddl-1, -1, 'o');
+            updatek = @(Xt) set(draw1, 'XData', [ddl-1+0.4*Xt(ddl) ddl-1+0.4*Xt(ddl)+sin(Xt(k))], 'YData', [0 -cos(Xt(k))]);
+%             set(draw2, 'XData', ddl-1+0.4*Xt(ddl)+0.4*sin(Xt(k)), 'YData', -0.4*cos(Xt(k)));
+            updates{end+1} = updatek;
+        end
+    end
+end
 
     function show()
-        for el=X
-            set (p1 , 'XData', 0.4*el);
+        for Xt=X.'
+            for kupdate = 1:length(updates)
+                update = updates{kupdate};
+                update(Xt);
+            end
             drawnow limitrate;
             pause(dT);
         end
--- test.m
+++ test.m
@@ -5,15 +5,18 @@
 % tour1.reponseLibreAvecTMD(0,1, 40);
 % tour2.reponseLibreAvecTMD([0 0],[1 0], 40);
 
-pendule = TMDpendule(9.81^2, 1, 9.81, @(theta,omega) 0*50*omega*(abs(theta)<5*pi/180));
+pendule = TMDpendule(9.81^2, 1, 9.81, @(theta,omega) 50*omega*(abs(theta)<5*pi/180));
 [t, x] = pendule.reponseLibre(pi/2-0.0001, 0, 200);
 tempsCaracteristique(t, x(:, 1))
 
-tour = Structure(50, 50, @(x,v) 0.0*v, {{pendule, 1}});
+pendule2 = TMDpendule(9.81^2, 2, 9.81, @(theta,omega) 50*omega*(abs(theta)<5*pi/180));
+
+tour = Structure(50, 50, @(x,v) 0.0*v, {{pendule, 1}, {pendule2, 1}});
+tour2 = Structure([50 0;0 50], [100 -50;-50 50], @(x,v) [0;0], {{pendule, 1}, {pendule2, 2}});
 
 % tour.reponseLibreSansTMD(0, 1, 200);
-[t, x] = tour.reponseLibreAvecTMD(0, 1, 200);
+[t, x] = tour.reponseLibreAvecTMD(0, 5, 200);
+[t, x] = tour2.reponseLibreAvecTMD([0 0], [5 0], 200);
 tempsCaracteristique(t, x(:, 1))
 
-
-animate(tour, t, x(:, 1));
+animate(tour2, t, x);
