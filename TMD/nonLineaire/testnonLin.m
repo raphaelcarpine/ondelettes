4ae069a111e03b8863ceef8ca5c4e4e516341c6b
CARPINE Raphael <carpine@ad.ifsttar.fr>
2019-03-27 19:12:41 +0100

    integration de la phase complexe

--- TMD/nonLineaire/testnonLin.m
+++ TMD/nonLineaire/testnonLin.m
@@ -1,5 +1,5 @@
 function testnonLin()
-% clear all;
+clear all;
 % close all;
 
 
@@ -8,7 +8,7 @@ mu = 0.01;
 omega0 = 2*pi;
 zeta0 = 0.0;
 omega1 = omega0/(1+mu);
-zeta1 = 0.0;
+zeta1 = 0.;
 
 T = 50;
 
@@ -42,12 +42,13 @@ plot(t, xout, 'Parent', ax);
 
 %%
 fun = @(t, angles) [angles(3); angles(4);...
-    1i*(omega0^2-angles(3)^2-mu*omega1^2*exp(1i*(angles(2)-angles(1))));...
-    1i*((1+mu)*omega1^2-angles(4)^2-omega0^2*exp(1i*(angles(1)-angles(2))))];
-angles0 = [1i*log(omega0) ; 1i*log(omega1)-pi; omega0; omega1];
+    1i*(omega0^2-angles(3)^2+1i*2*zeta0*omega0*angles(3)-mu*omega1^2*exp(1i*(angles(2)-angles(1))));...
+    1i*((1+mu)*omega1^2-angles(4)^2+1i*2*zeta1*omega1*angles(4)-omega0^2*exp(1i*(angles(1)-angles(2))))];
+
+angles0 = [1i*log(omega0) ; 1i*log(omega1)+pi; omega0; omega1*0];
 
 
-[t, xout] = differentialEq(angles0, fun, T, false, 'MaxStep', 1e-2);
+[t, xout] = differentialEq(angles0, fun, T, false, 'MaxStep', 1e-3);
 
 xout = xout(:,1);
 xout = abs(exp(1i*xout));
