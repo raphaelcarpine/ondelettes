19bf87d9ff130d258d967b230ff18dae19bb68f0
CARPINE Raphael <carpine@ad.ifsttar.fr>
2019-04-02 17:18:44 +0200

    update

--- TMD/nonLineaire/nonLin1ddl.m
+++ TMD/nonLineaire/nonLin1ddl.m
@@ -8,6 +8,6 @@ d2x = '-omega0^2*x-(abs(x)<=delta)*epsilon*sign(dx)*abs(dx)^n';
 T = 100;
 
 
-systemeQuelconque({'x'}, {d2x}, {'omega0', 'epsilon', 'n', 'delta'}, {omega0, eta, n, delta}, 0, 1, 'T', T);
+systemeQuelconque({'x'}, {d2x}, {'omega0', 'epsilon', 'n', 'delta'}, {omega0, eta, n, delta}, 0, 1, false, 'T', T);
 
 RegressionMenu;
