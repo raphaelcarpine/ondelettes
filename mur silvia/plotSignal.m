9b62779bd7a1fc9c2fc2019bd1843b3e1d6b0a11
raphael carpine <raphael.carpine@gmail.com>
2021-01-12 14:12:41 +0100

    'update'

--- mur silvia/plotSignal.m
+++ mur silvia/plotSignal.m
@@ -1,12 +1,12 @@
-P = 7;
+P = 6;
 
 %%
 [t, X] = getData(P, 0);
 
-figure;
+figure('Name', sprintf('Acceleration data, P%u', P));
 plot(t, X);
 xlabel('Time [s]');
 ylabel('Acceleration [m/s²]');
-xlim([5 65]);
+% xlim([5 65]);
 l = [char(ones(9, 1) * 'CH'), num2str((1:9)')];
 legend(l, 'NumColumns', 3, 'Location', 'best');
--- simulations nonlin/nonlin1.m
+++ simulations nonlin/nonlin1.m
@@ -1,3 +1,4 @@
+%% system
 m = 1;
 k1 = (10*2*pi)^2;
 k2 = k1/2;
@@ -5,4 +6,44 @@ x0 = 1;
 c = 0.1;
 
 
-k = @(x) k1*x*(x<=x0) + k2*x*(x>x0);
+kx = @(x) k1*x*(x<=x0) + (k2*(x-x0) + k1*x0)*(x>x0);
+
+%% time
+T = 50;
+fe = 500;
+
+dt = 1/fe;
+t = 0:dt:T;
+
+%% excitation
+
+% dirac
+f = zeros(size(t));
+% f(1) = 1 / dt;
+
+% noise
+
+% noise + diracs
+
+
+%% ode
+x0 = 3;
+v0 = 0;
+
+X0 = [x0; v0];
+
+D = @(t, X) [X(2); 1/m*(-kx(X(1)) - c*X(2) - f(floor(t/dt)+1))];
+
+[~, X] = ode45(D, t, X0);
+x = X(:, 1).';
+
+%% plot
+figure;
+plt = plot(t, x);
+xlabel('Time [s]');
+ylabel('Displacement [m]');
+
+%% wavelet
+fmin = 5;
+fmax = 50;
+Q = 10;
